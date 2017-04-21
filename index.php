ece448b986787d9694835f6945deeecbf8447c37
Petter Alnes <petter.99@live.no>
2017-04-21 12:38:41 +0200

    omvendt fargepalett

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Sidetittel</title>
+        <title>Bespist</title>
         <meta charset="utf-8" />
 		<link rel="stylesheet" href="nettsidestil.css">
+		<link rel="icon" href="bespistlogo_b.png">
 
     </head>
     <body>
 		<div id="header">
-			<img id="bilde" alt="logo" src="bespistlogo.png">
+			<img id="bilde" alt="logo" src="bespistlogo2.png">
 			<nav id="navigasjon">
 				<div class="meny">MENY</div>
 				<div class="meny">MENY</div>
@@ -16,6 +17,6 @@
 				<div class="meny">MENY</div>
 			</nav>
 		</div>
-		<div id="ball"></div>
+		<div id="ball"> <img id="B_bilde" src="bespistlogo_b.png" alt="B"> </div>
     </body>
 </html>
