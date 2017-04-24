5ad489e3f8aae2715c47175641c1b609023565fe
Petter Alnes <petter.99@live.no>
2017-04-24 15:22:13 +0200

    Header  nr 1 ferdigstilt

--- index.php
+++ index.php
@@ -4,19 +4,19 @@
         <title>Bespist</title>
         <meta charset="utf-8" />
 		<link rel="stylesheet" href="nettsidestil.css">
-		<link rel="icon" href="bespistlogo_b.png">
+		<link rel="icon" href="bespist.png">
 
     </head>
     <body>
 		<div id="header">
 			<img id="bilde" alt="logo" src="bespistlogo2.png">
 			<nav id="navigasjon">
-				<div class="meny">MENY</div>
-				<div class="meny">MENY</div>
-				<div class="meny">MENY</div>
-				<div class="meny">MENY</div>
+				<a><div class="meny"><p>MENY</p></div></a>
+				<a><div class="meny"><p>MENY</p></div></a>
+				<a><div class="meny"><p>MENY</p></div></a>
 			</nav>
 		</div>
 		<div id="ball"> <img id="B_bilde" src="bespistlogo_b.png" alt="B"> </div>
+		<main></main>
     </body>
 </html>
