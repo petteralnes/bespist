25a49f1970262b7f915e2ad0986e4d82309307ba
Petter Alnes <petter.99@live.no>
2017-04-25 13:05:57 +0200

    la til bildesystem + css-system

--- bestilling.php
+++ bestilling.php
@@ -14,19 +14,19 @@ ini_set('display_errors', 'On');
         <title>Bespist</title>
         <meta charset="utf-8" />
 		<link rel="stylesheet" href="nettsidestil.css">
-		<link rel="icon" href="bespist.png">
+		<link rel="icon" href="img/bespist.png">
 
     </head>
     <body>
 		<div id="header">
-			<a href="index.php"><img id="bilde" alt="logo" src="bespistlogo2.png"></a>
+			<a href="index.php"><img id="bilde" alt="logo" src="img/bespistlogo2.png"></a>
 			<nav id="navigasjon">
 				<a><div class="meny"><p>MENY</p></div></a>
 				<a><div class="meny"><p>MENY</p></div></a>
 				<a><div class="meny"><p>Bestilling</p></div></a>
 			</nav>
 		</div>
-		<div id="ball"> <img id="B_bilde" src="bespistlogo_b.png" alt="B"> </div>
+		<div id="ball"> <img id="B_bilde" src="img/bespistlogo_b.png" alt="B"> </div>
 		<main>
 			<table>
 				<tr>
@@ -41,7 +41,7 @@ ini_set('display_errors', 'On');
 					<td><?php echo $rad["matrettid"]; ?></td>
 					<td><?php echo $rad["matrettnavn"]; ?></td>
 					<td><?php echo $rad["beskrivelse"]; ?></td>
-					<td><img src="/img<?php echo $rad["matbilde"]; ?>" alt="bilde"></td>
+					<td><img src="img/<?php echo $rad["matbilde"]; ?>" alt="bilde"></td>
 			  	</tr>
 				<?php } ?>
 			</table>
--- index.php
+++ index.php
@@ -4,19 +4,19 @@
         <title>Bespist</title>
         <meta charset="utf-8" />
 		<link rel="stylesheet" href="nettsidestil.css">
-		<link rel="icon" href="bespist.png">
+		<link rel="icon" href="img/bespist.png">
 
     </head>
     <body>
 		<div id="header">
-			<a href="index.php"><img id="bilde" alt="logo" src="bespistlogo2.png"></a>
+			<a href="index.php"><img id="bilde" alt="logo" src="img/bespistlogo2.png"></a>
 			<nav id="navigasjon">
 				<a><div class="meny"><p>MENY</p></div></a>
 				<a><div class="meny"><p>MENY</p></div></a>
 				<a href="bestilling.php"><div class="meny"><p>Bestilling</p></div></a>
 			</nav>
 		</div>
-		<div id="ball"> <img id="B_bilde" src="bespistlogo_b.png" alt="B"> </div>
+		<div id="ball"> <img id="B_bilde" src="img/bespistlogo_b.png" alt="B"> </div>
 		<main>
 
 		</main>
